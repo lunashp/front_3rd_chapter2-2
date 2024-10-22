0efba2e59362483d22fc803b1964ace3957e0f7f
lunashp <hijnshp@gmail.com>
2024-10-22 23:31:03 +0900

    FEAT: basic test통과를 위한 product id 추가

--- src/refactoring/components/ProductComponent.tsx
+++ src/refactoring/components/ProductComponent.tsx
@@ -15,7 +15,11 @@ export const ProductComponent = ({
   const remainingStock = getRemainingStock(product, cart);
 
   return (
-    <div className="bg-white p-3 rounded shadow">
+    <div
+      key={product.id}
+      data-testid={`product-${product.id}`}
+      className="bg-white p-3 rounded shadow"
+    >
       <div className="flex justify-between items-center mb-2">
         <span className="font-semibold">{product.name}</span>
         <span className="text-gray-600">
